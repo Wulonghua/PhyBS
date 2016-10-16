591fcfa9cb203486783f44395886a7f8c4081d94
WuLonghua <wu.2724@osu.edu>
2016-10-16 11:12:19 -0400

    alter the structure

--- FVM/IsotropicMaterial.h
+++ FVM/IsotropicMaterial.h
@@ -4,8 +4,10 @@
 #include <Eigen\Dense>
 #include <Eigen\Core>
 #include <Eigen\svd>
+#include <Eigen\Sparse>
 #include <vector>
 #include <memory>
+#include <array>
 
 #include "TetMesh.h"
 
@@ -19,13 +21,15 @@ public:
 	virtual Eigen::Vector3d computeEnergy2InvariantsGradient(int tetID, Eigen::Vector3d invariants)=0;
 	virtual Eigen::Matrix3d computeEnergy2InvariantsHessian(int tetID, Eigen::Vector3d invariants)=0;
 
+	Eigen::MatrixXd computeInnerForcesfromFhats();
+
+	// compute stiffness Matrix
+	Eigen::MatrixXd computeStiffnessMatrix(int tetID);
+	Eigen::SparseMatrix<double> computeGlobalStiffnessMatrix();
+
 	const double m_eps_singularvalue;
 
 protected:
-	//see [Teran. 2004], compute F_hat and make sure U,V are real rotation matrix.
-	void computeSVD33modified(Eigen::Matrix3d F, Eigen::Vector3d &S, Eigen::Matrix3d &U, Eigen::Matrix3d &V);
-	void computeFhatsInvariants();
-
 	std::vector<double> m_mus;
 	std::vector<double> m_lambdas;
 	std::shared_ptr<TetMesh> m_tetModel;
@@ -33,5 +37,16 @@ protected:
 	Eigen::MatrixXd m_Us;
 	Eigen::MatrixXd m_Vs;
 	Eigen::MatrixXd m_Invariants;
+
+private:
+	//see [Teran. 2004], compute F_hat and make sure U,V are real rotation matrix.
+	void computeSVD33modified(Eigen::Matrix3d F, Eigen::Vector3d &S, Eigen::Matrix3d &U, Eigen::Matrix3d &V);
+	void computeFhatsInvariants();
+
+	// compute dP/dF
+	Eigen::MatrixXd computeDP2DF(int tetID);
+	Eigen::Matrix3d restoreMatrix33fromTeranVector(Eigen::VectorXd v);
+
+	const std::array<int, 9> m_matrix33fromTeran; // transfer teran's order to 3*3 matrix row major
 };
 
--- FVM/IsotropicNeohookeanMaterial.h
+++ FVM/IsotropicNeohookeanMaterial.h
@@ -1,10 +1,8 @@
 #pragma once
 #include "IsotropicMaterial.h"
 #include <Eigen\Dense>
-#include <Eigen\Sparse>
 #include <algorithm>
 #include <cmath>
-#include <array>
 
 class IsotropicNeohookeanMaterial :
 	public IsotropicMaterial
@@ -16,16 +14,5 @@ public:
 	virtual Eigen::Vector3d computeEnergy2InvariantsGradient(int tetID, Eigen::Vector3d invariants);
 	virtual Eigen::Matrix3d computeEnergy2InvariantsHessian(int tetID, Eigen::Vector3d invariants);
 
-	Eigen::MatrixXd computeInnerForcesfromFhats();
-
-	// compute stiffness Matrix
-	Eigen::MatrixXd computeStiffnessMatrix(int tetID);
-	Eigen::SparseMatrix<double> computeGlobalStiffnessMatrix();
-
-private:
-	// compute dP/dF
-	Eigen::MatrixXd computeDP2DF(int tetID);
-	Eigen::Matrix3d restoreMatrix33fromTeranVector(Eigen::VectorXd v);
-	const std::array<int,9> m_matrix33fromTeran; // transfer teran's order to 3*3 matrix row major
 };
 
