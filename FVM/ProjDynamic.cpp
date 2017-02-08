5173e773aceba1f4dde48827aab16e7292d0ab50
Wulonghua <wu.2724@osu.edu>
2017-02-08 17:12:50 -0500

    finish local projection

--- FVM/ProjDynamic.cpp
+++ FVM/ProjDynamic.cpp
@@ -68,10 +68,12 @@ void ProjDynamic::buildGlobalSolverMatrix(const Eigen::VectorXf &node_mass, cons
 			}
 		}
 	}
+	m_globalSolverMat.setFromTriplets(triList.begin(), triList.end());
+	m_pardiso_solver.compute(m_globalSolverMat);
 }
 
 Eigen::VectorXf ProjDynamic::projectLocalConstraints(const Eigen::VectorXf & node_mass, const Eigen::VectorXf &node_inv_mass,
-													 const Eigen::MatrixXi &tets, float t, const Eigen::MatrixXf &pos,
+													 const Eigen::MatrixXi &tets, float t, const Eigen::MatrixXf &pos, const Eigen::MatrixXf &Dm_inverse,
 													 const Eigen::MatrixXf &vel, const Eigen::MatrixXf & fext)
 {
 	Eigen::MatrixXf s = Eigen::MatrixXf::Zero(3, n_nodes);
@@ -86,4 +88,45 @@ Eigen::VectorXf ProjDynamic::projectLocalConstraints(const Eigen::VectorXf & nod
 	{
 		s.col(i) = s.col(i)*node_mass[i] / (t*t);
 	}
+	
+	Eigen::MatrixXf AcT(9, 12);
+	Eigen::Matrix3f Ds, F, R, DmInvT;
+	Eigen::Vector3f tmp;
+	Eigen::Vector4i v;  // nodes' indices
+	Eigen::VectorXf Rv,c;
+	Rv.resize(9);
+
+	for (int k = 0; k < n_tets; ++k)
+	{
+		Ds.col(0) = pos.col(tets(1, k)) - pos.col(tets(0, k));
+		Ds.col(1) = pos.col(tets(2, k)) - pos.col(tets(0, k));
+		Ds.col(2) = pos.col(tets(3, k)) - pos.col(tets(0, k));
+
+		F = Ds * Dm_inverse.block<3, 3>(0, 3*k);
+		Eigen::JacobiSVD<Eigen::Matrix3f> svd(F, Eigen::ComputeFullU | Eigen::ComputeFullV);
+		R = svd.matrixU() * svd.matrixV().transpose();
+		Rv(0) = R(0, 0); Rv(1) = R(0, 1); Rv(2) = R(0, 2);
+		Rv(3) = R(1, 0); Rv(4) = R(1, 1); Rv(5) = R(1, 2);
+		Rv(6) = R(2, 0); Rv(7) = R(2, 1); Rv(8) = R(2, 2);
+
+		DmInvT = Dm_inverse.block<3, 3>(0, k * 3).transpose();
+		v = tets.col(k);
+		tmp = -1.0 * DmInvT.rowwise().sum();
+		AcT.setZero();
+		AcT.block<3, 1>(0, 0) = AcT.block<3, 1>(3, 1) = AcT.block<3, 1>(6, 2) = tmp;
+		AcT.block<3, 1>(0, 3) = AcT.block<3, 1>(3, 4) = AcT.block<3, 1>(6, 5) = DmInvT.col(0);
+		AcT.block<3, 1>(0, 6) = AcT.block<3, 1>(3, 7) = AcT.block<3, 1>(6, 8) = DmInvT.col(1);
+		AcT.block<3, 1>(0, 9) = AcT.block<3, 1>(3, 10) = AcT.block<3, 1>(6, 11) = DmInvT.col(2);
+		AcT.transposeInPlace();
+
+		c = m_stiffWeight * AcT * Rv;
+
+		for (int i = 0; i < 4; ++i)
+		{
+			s.col(v(i)) += c.block<3, 1>(i * 3, 0);
+		}
+	}
+
+	s.resize(3*n_nodes,1);
+	return s;
 }
--- FVM/ProjDynamic.h
+++ FVM/ProjDynamic.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <Eigen/Dense>
 #include <Eigen/Sparse>
+#include <Eigen/svd>
+#include <Eigen/PardisoSupport>
 #include <vector>
 #include <memory>
 
@@ -14,11 +16,12 @@ public:
 
 	void buildGlobalSolverMatrix(const Eigen::VectorXf & node_mass, const Eigen::MatrixXi &tets, float t, const Eigen::MatrixXf &Bm);
 	Eigen::VectorXf projectLocalConstraints(const Eigen::VectorXf & node_mass, const Eigen::VectorXf &node_inv_mass,
-								const Eigen::MatrixXi &tets, float t, const Eigen::MatrixXf &pos,
+								const Eigen::MatrixXi &tets, float t, const Eigen::MatrixXf &pos, const Eigen::MatrixXf &Dm_inverse,
 								const Eigen::MatrixXf &vel, const Eigen::MatrixXf & fext);
 private:
 
 	Eigen::SparseMatrix<float, Eigen::RowMajor> m_globalSolverMat;
+	Eigen::PardisoLDLT<Eigen::SparseMatrix<float, Eigen::RowMajor>> m_pardiso_solver;
 	Eigen::VectorXf m_stiffWeight;
 	float m_stiffness;
 
